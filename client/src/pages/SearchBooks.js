6d910ed1d3dec5c96b911a5001673e955ad42299
okingdomz <rkbarnes0310@gmail.com>
2023-01-23 14:44:29 -0600

    client change

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-import { useMutation } from '@apollo/react-hooks';
+import { useMutation } from '@apollo/client';
 import {SAVE_BOOK} from '../utils/mutations';
 // import {GET_ME} from '../utils/queries';
 import Auth from '../utils/auth';
